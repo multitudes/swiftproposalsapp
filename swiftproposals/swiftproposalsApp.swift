894e5eaad9ce32b03ae0d4eed0e96dc697bcca86
Laurent Brusa <wrmultitudes@gmail.com>
2023-08-13 13:27:00 +0200

    first

---
+++ swiftproposals/swiftproposalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftproposalsApp.swift
+//  swiftproposals
+//
+//  Created by Laurent B on 13/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftproposalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
